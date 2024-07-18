fa70d55078823cff1d955543abb9d59c48af025d
angel <angel@dvaqued>
2024-07-18 08:47:30 -0600

    Uso de github en netbeans.

---
+++ src/programacion1_q3/DatoAlumnos.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package programacion1_q3;
+
+/**
+ *
+ * @author angel
+ */
+public class DatoAlumnos {
+    public static void main(String []args) {
+    //Cometario de línea
+    /*
+    Comentario
+    de 
+    Bloque
+    */
+    /*
+    Tema: Variable
+    Definición: Es un contenedor de datos
+    
+    Tipos de datos
+    1- Númericos
+        1.1 Entero - int,short,long
+        1.2 Décimal - double
+    2-Caracter - char
+    3-Cadena de caracteres - String
+    4 - Lógicos - boolean
+    Sintataxis
+    Tipo de Datos identificador(Nombre de la variable);
+    */
+    
+    }
+}
